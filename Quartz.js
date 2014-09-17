e74528372c1693982f2c8daf3473243ded13dd4c
Guy Ellis <wildfiction@gmail.com>
2014-09-17 14:33:41 -0700

    Added tests to run directly against the API.

--- Quartz.js
+++ Quartz.js
@@ -1,21 +1,21 @@
 
-var kue = require('kue')
-var _ = require('underscore')
+var kue = require('kue');
+var _ = require('underscore');
 
-var util = require('util')
-var EventEmitter = require('events').EventEmitter
+var util = require('util');
+var EventEmitter = require('events').EventEmitter;
 
-var Server = require('./lib/Server.js')
-var Scheduler = require('./lib/Scheduler.js')
+var Server = require('./lib/Server.js');
+var Scheduler = require('./lib/Scheduler.js');
 
 function Quartz(config) {
-  EventEmitter.call(this)
+  EventEmitter.call(this);
 
   config = _.extend({
     queue: {
       redis: {
         port: 6379,
-        host: '127.0.0.1',
+        host: '127.0.0.1'
       }
     },
     listen: 8001,
@@ -23,45 +23,45 @@ function Quartz(config) {
     quartzURL: 'http://127.0.0.1:8080/api',
     concurrency: 5,
     monitor: false
-  }, config || {})
+  }, config || {});
 
-  var self = this
+  var self = this;
 
-  this._queue = kue.createQueue(config.queue)
+  this._queue = kue.createQueue(config.queue);
 
   var app;
   if(config.monitor) {
     app = kue.app
   }
 
-  this._server = new Server({port: config.listen, queue: this._queue, app: kue.app})
-  this._scheduler = new Scheduler({queue: this._queue, callbackURL: config.callbackURL, quartzURL: config.quartzURL})
+  this._server = new Server({port: config.listen, queue: this._queue, app: kue.app});
+  this._scheduler = new Scheduler({queue: this._queue, callbackURL: config.callbackURL, quartzURL: config.quartzURL});
 
   this._queue.process('job', config.concurrency || 5, function(job, done) {
-    console.log('processing job [', job.data.name, '] now')
+    console.log('processing job [', job.data.name, '] now');
     self.emit(job.data.name, job.data.data, done)
   })
 }
 
-util.inherits(Quartz, EventEmitter)
+util.inherits(Quartz, EventEmitter);
 
 Quartz.prototype.schedule = function(jobName, date, data, callback) {
   if(_.isNumber(date)) {
     date = new Date(date)
   }
   this._scheduler.schedule(jobName, date, data, callback)
-}
+};
 
 Quartz.prototype.cancel = function(jobId, callback) {
   this._scheduler.cancel(jobId, callback)
-}
+};
 
 Quartz.prototype.start = function(callback) {
   this._server.start(callback)
-}
+};
 
 Quartz.prototype.shutdown = function() {
   this._server.shutdown()
-}
+};
 
-module.exports = Quartz
+module.exports = Quartz;
--- test/test.js
+++ test/test.js
@@ -1,31 +1,101 @@
-var assert = require('assert')
+var assert = require('assert');
+var request = require('request');
+var Quartz = require('../Quartz.js');
+var debug = require('debug')('test:node-quartz');
+var moment = require('moment');
 
-var Quartz = require('../Quartz.js')
+var config = {
+  quartzURL: 'http://127.0.0.1:8090/scheduler/api'
+};
 
 describe('quartz-scheduler', function() {
 
-  var scheduler = new Quartz()
+  describe('jetty/quartz setup check', function() {
 
-  it('start', function(done) {
-    scheduler.start(done)
-  })
+    it('should check to see if the quartz scheduler is running at the given address', function (done) {
+      this.timeout(5000);
+      request.get(config.quartzURL, function (error, response, body) {
+        if(error) {
+          debug('Error in request.get:');
+          debug(error);
+        }
+        assert.ok(!error);
+        // Expect a 405 back from the call because the service does not
+        // support the HTTP GET verb - this is correct behavior.
+        assert.equal(response.statusCode, 405, 'Do you have the jetty/quartz server running?');
+        debug(body);
+        done();
+      });
+    });
 
-  it('schedule', function(done) {
-    var when = Date.now() + 2000
-    scheduler.schedule('testTask', when, {hello: "world"}, function(err, jobId) {
-      done(err)
+    var jobKey;
+    it('should schedule a job and return a group and name', function (done) {
+      var payload = {
+        name: 'Test Job Name',
+        data: {color: 'blue'}
+      };
+
+      var body = {
+        "url": "http://localhost:3000",
+        timestamp: moment().add(10, 'minutes').valueOf(),
+        payload: JSON.stringify(payload)
+      };
+
+      request.post({url: config.quartzURL, json: body}, function (error, response, body) {
+        if(error) {
+          debug('Error in request.post:');
+          debug(error);
+        }
+        assert.ok(!error, error);
+        debug('body:');
+        debug(body);
+        assert.equal(response.statusCode, 200);
+        assert(body.key);
+        var parts = body.key.split('::');
+        assert.equal(parts[0], 'http');
+        // TODO: assert isUUID for parts[1]
+        jobKey = body; // should be JSON
+        done();
+      });
+    });
+
+    it('should cancel a job and return success', function (done) {
+      var deleteUrl = config.quartzURL + '/' + jobKey.key;
+      request.del({url:deleteUrl}, function (error, response, body) {
+        assert.ok(!error, error);
+        assert.equal(response.statusCode, 200);
+        debug('body:');
+        debug(body);
+        done();
+      });
+    });
+
+  });
+
+  describe('scheduler functionality', function() {
+    var scheduler = new Quartz(config);
+
+    it('start', function (done) {
+      scheduler.start(done)
+    });
+
+    it('schedule', function (done) {
+      var when = Date.now() + 2000;
+      scheduler.schedule('testTask', when, {hello: "world"}, function (err/*, jobId*/) {
+        done(err)
+      })
+    });
+
+    it('execute', function (done) {
+      this.timeout(5000);
+      scheduler.on('testTask', function (data, callback) {
+        assert.ok(data);
+        assert.ok(data.hello);
+        assert.equal(data.hello, 'world');
+        callback();
+        done();
+      })
     })
-  })
+  });
 
-  it('execute', function(done) {
-    this.timeout(5000)
-    scheduler.on('testTask', function(data, callback) {
-      assert.ok(data)
-      assert.ok(data.hello)
-      assert.equal(data.hello, 'world')
-      callback()
-      done()
-    })
-  })
-
-})
+});
